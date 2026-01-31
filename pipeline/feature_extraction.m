93d503904ae16dbeafa69c8dd183c672ba18bc2a
mdsaif48 <mdsaif21600172@gmail.com>
2026-01-31 15:58:59 +0530

    Clean up comments in feature_extraction.m
    Removed comments for time-domain and FFT features.

--- pipeline/feature_extraction.m
+++ pipeline/feature_extraction.m
@@ -1,13 +1,9 @@
 for k = 1:numFiles
     sig = dataStruct(k).vibrationFiltered;
-    
-    % Time-domain features
     dataStruct(k).RMS   = rms(sig);
     dataStruct(k).STD   = std(sig);
     dataStruct(k).Skew  = skewness(sig);
     dataStruct(k).Kurt  = kurtosis(sig);
-    
-    % FFT features (magnitude spectrum)
     N = length(sig);
     Y = fft(sig);
     f = (0:N-1)*(fs/N);
